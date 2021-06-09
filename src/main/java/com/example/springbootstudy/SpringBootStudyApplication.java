ee3ce3b21bf58ff19cd00a84cb591421d198df6f
quyixiao <2621048238@qq.com>
2021-06-09 17:48:11 +0800

    提交修改

--- src/main/java/com/example/springbootstudy/SpringBootStudyApplication.java
+++ src/main/java/com/example/springbootstudy/SpringBootStudyApplication.java
@@ -7,11 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class SpringBootStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(SpringBootStudyApplication.class);
+		Map<String, Object> defaultProperties = new HashMap<>();
+
+		defaultProperties.put("pageNum",1);
+		defaultProperties.put("pageSize",20);
+		springApplication.setDefaultProperties(defaultProperties);
+
 		springApplication.setBannerMode(Banner.Mode.CONSOLE);
 		springApplication.run(args);
 		//SpringApplication.run(SpringBootStudyApplication.class, args);
--- src/main/java/com/example/springbootstudy/controller/TestController.java
+++ src/main/java/com/example/springbootstudy/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.example.springbootstudy.controller;
 
 import com.example.springbootstudy.service.HelloService;
+import com.example.springbootstudy.utils.SpringContextUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +15,22 @@ public class TestController {
     private HelloService helloService;
 
     @RequestMapping("/home")
-    public String home(){
+    public String home() {
         return helloService.sayHello();
+    }
+
+
+    @RequestMapping("/query")
+    public String home(Integer pageNum, Integer pageSize) {
+        if (pageNum == null) {
+            ApplicationContext applicationContext = SpringContextUtils.getApplicationContext();
+            Environment environment = applicationContext.getEnvironment();
+            pageNum = Integer.parseInt(environment.getProperty("pageNum"));
+            pageSize = Integer.parseInt(environment.getProperty("pageSize"));
+            System.out.println("前端没有设置pageNum,pageSize ，使用系统默认的配置参数");
+        }
+        System.out.println("====pageNum==" + pageNum + ", pageSize = " + pageSize);
+        return "SUCESS";
 
     }
 }
